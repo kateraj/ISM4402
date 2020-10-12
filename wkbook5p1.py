2215211ce58abe90636ceee08b0c4015ad7f997d
kateraj <60445317+kateraj@users.noreply.github.com>
2020-10-11 23:30:41 -0400

    Add files via upload

---
+++ wkbook5p1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+Location = "datasets/gradedata.csv"
+df = pd.read_csv(Location)
+df.tail()
+df.take(np.random.permutation(len(df))[:500])
+
