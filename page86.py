afce9f47034ad613c675165ea1fd0b4171bcc806
kateraj <60445317+kateraj@users.noreply.github.com>
2020-11-01 23:52:29 -0500

    Add files via upload

---
+++ page86.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+names = ['Bob','Jessica','Mary','John','Mel']
+startangle= John
+
